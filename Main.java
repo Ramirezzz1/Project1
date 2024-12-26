e3fa347a2c78418a84c9867ccb98f9f23b92273f
Ramirezzz1 <ronaldramirezzz1@gmail.com>
2024-12-26 18:38:12 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+
+public class Main {
+
+    static int N = 8; // variable to store the size chess board 
+    static int[][] moves = {{1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}}; //2D array to store all possible moves for the knight
+
+    public static void main(String[] args) {
+        int[][] board = new int[N][N]; // 2D array to store the chess board
+
+        if (solveKnightTour(board, 0, 0, 1)) {
+            printBoard(board);
+        } else {
+            System.out.println("Solution does not exist");
+        }
+    }
+
+    // Function to check if the move is valid
+    static boolean isValidMove(int x, int y, int[][]board){
+        // Check if the move is within the board
+    }
+
+    // Function to solve the knight tour problem
+    static boolean solveKnightTour(int[][]board,int x, int y, int moveCount){
+        // Check if the move is valid
+        // Check if the move is the last move
+        // Check if the move is not the last move
+    }
+
+    // Function to print the chess board
+    static void printBoard(int[][] board) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
